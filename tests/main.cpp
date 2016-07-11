2195ac5ab96595aa01d9d143db64c2924c04d1e6
niXman <niXman@users.noreply.github.com>
2016-07-11 07:32:00 +0300

    Update main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,6 +3,7 @@
 #include <boost/test/unit_test.hpp>
 
 // Copyright Alexei Zakharov, 2013.
+// Copyright niXman (i dot nixman dog gmail dot com) 2016.
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
